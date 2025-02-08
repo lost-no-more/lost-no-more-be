71876424c5d516f5b0efc2a315d81ac3eda2da02
dongyun <ehddbs452100@naver.com>
2025-02-09 01:41:36 +0900

    [feat] #4 subscribe 관련 domain, controller, service, repository 기본 설정

---
+++ src/main/java/org/lostnomore/backend/subscribe/domain/Subscribe.java
@@ -0,0 +1,37 @@
+package org.lostnomore.backend.subscribe.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import org.lostnomore.backend.global.domain.BaseEntity;
+import org.lostnomore.backend.item.domain.Category;
+import org.lostnomore.backend.user.domain.User;
+
+@Getter
+@Table(name = "subscribe")
+@Entity
+public class Subscribe extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subscribe_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(name = "keyword", nullable = false)
+    private String keyword;
+}
