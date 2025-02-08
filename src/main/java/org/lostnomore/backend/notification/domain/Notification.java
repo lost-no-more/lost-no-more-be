90faeafc9c599136b07fdafd5446b0d578e1735c
dongyun <ehddbs452100@naver.com>
2025-02-09 01:43:11 +0900

    [feat] #4 notification 관련 domain, controller, service, repository 기본 설정

---
+++ src/main/java/org/lostnomore/backend/notification/domain/Notification.java
@@ -0,0 +1,42 @@
+package org.lostnomore.backend.notification.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Getter;
+import org.lostnomore.backend.global.domain.BaseEntity;
+import org.lostnomore.backend.item.domain.Category;
+
+@Getter
+@Table(name = "notification")
+@Entity
+public class Notification extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "total_count", nullable = false)
+    private Long totalCount;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "date", nullable = false)
+    private String keyword;
+
+    @Column(name = "ids", nullable = false)
+    private String ids;
+}
---
+++ src/main/java/org/lostnomore/backend/notification/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package org.lostnomore.backend.notification.repository;
+
+import org.lostnomore.backend.notification.domain.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+}
