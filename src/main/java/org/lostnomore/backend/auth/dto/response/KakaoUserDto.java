0d04fb8681224f043b1ab22082e5d9154e78e4aa
dongyun <ehddbs452100@naver.com>
2025-02-10 20:56:52 +0900

    [feat] #9 소셜 로그인 요청 링크, Access Token, 각 소셜로그인의 사용자 정보 DTO

---
+++ src/main/java/org/lostnomore/backend/auth/dto/response/KakaoUserDto.java
@@ -0,0 +1,16 @@
+package org.lostnomore.backend.auth.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class KakaoUserDto {
+
+    @JsonProperty("kakao_account")
+    KakaoAccount kakaoAccount;
+}
