31d5fdb64b356b0e3f2bf42854b4df2c8299c363
dongyun <ehddbs452100@naver.com>
2025-02-10 21:01:48 +0900

    [feat] #9 사용자 관련 Repository

--- src/main/java/org/lostnomore/backend/user/repository/UserRepository.java
+++ src/main/java/org/lostnomore/backend/user/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package org.lostnomore.backend.user.repository;
 
+import java.util.Optional;
+import org.lostnomore.backend.user.domain.SocialType;
 import org.lostnomore.backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByEmailAndSocialType(String email, SocialType socialType);
 }
