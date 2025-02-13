93f7fe8a4cb7803874b75af7608b7617eeacbeb0
dongyun <ehddbs452100@naver.com>
2025-02-14 00:44:21 +0900

    [feat] #21 로그인 성공 할 시 토큰 발급 및 토큰 만료 시 토큰 재발급 Controller

--- src/main/java/org/lostnomore/backend/auth/controller/AuthController.java
+++ src/main/java/org/lostnomore/backend/auth/controller/AuthController.java
@@ -1,13 +1,18 @@
 package org.lostnomore.backend.auth.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.lostnomore.backend.auth.dto.UserTokens;
+import org.lostnomore.backend.auth.provider.CookieProvider;
 import org.lostnomore.backend.auth.service.AuthService;
 import org.lostnomore.backend.global.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final CookieProvider cookieProvider;
 
     @GetMapping("/oauth/{provider}/code")
     public ResponseEntity<ResponseDto> getCodeLink(@PathVariable String provider) {
@@ -26,8 +32,20 @@ public class AuthController {
     }
 
     @PostMapping("/oauth/{provider}/login")
-    public ResponseEntity<ResponseDto> oauth(@PathVariable String provider, @RequestParam String code) {
-        String loginLink = authService.oauthLogin(provider, code);
-        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.success(loginLink));
+    public ResponseEntity<ResponseDto> oauth(@PathVariable String provider, @RequestParam String code,
+                                             HttpServletResponse response) {
+        UserTokens userTokens = authService.oauthLogin(provider, code);
+        cookieProvider.createCookie(userTokens.getRefreshToken(), response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.success(userTokens.getAccessToken()));
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<ResponseDto> reissue(@CookieValue("refresh-token") final String refreshToken,
+                                             @RequestHeader("Authorization") final String authorizationHeader,
+                                             HttpServletResponse response) {
+
+        UserTokens userTokens = authService.reissue(refreshToken, authorizationHeader);
+        cookieProvider.createCookie(userTokens.getRefreshToken(), response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.success(userTokens.getAccessToken()));
     }
 }
