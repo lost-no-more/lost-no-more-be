1cc1cfcbd51a13b34631dd65a7a098cbc7e3d844
dongyun <ehddbs452100@naver.com>
2025-02-14 00:53:23 +0900

    [test] #21 JWT 토큰 관련 테스트 코드

--- src/test/java/org/lostnomore/backend/auth/controller/AuthControllerTest.java
+++ src/test/java/org/lostnomore/backend/auth/controller/AuthControllerTest.java
@@ -1,21 +1,27 @@
 package org.lostnomore.backend.auth.controller;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.Test;
+import org.lostnomore.backend.auth.dto.UserTokenDto;
+import org.lostnomore.backend.auth.provider.CookieProvider;
 import org.lostnomore.backend.auth.provider.OAuthProvider;
 import org.lostnomore.backend.auth.service.AuthService;
 import org.lostnomore.backend.common.ControllerTest;
 import org.lostnomore.backend.global.exception.BusinessException;
 import org.lostnomore.backend.global.exception.code.AuthErrorCode;
-import org.lostnomore.backend.global.exception.code.CommonErrorCode;
+import org.lostnomore.backend.global.exception.code.BusinessErrorCode;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @WebMvcTest(AuthController.class)
@@ -27,8 +33,14 @@ class AuthControllerTest extends ControllerTest {
     @MockitoBean
     private AuthService authService;
 
+    @MockitoBean
+    private CookieProvider cookieProvider;
+
     @InjectMocks
     private AuthController authController;
+
+    private final static String REFRESH_TOKEN = "refreshToken";
+    private final static String ACCESS_TOKEN = "accessToken";
 
     @Test
     void OAuth_소셜_로그인을_위한_링크와_상태코드_200을_반환() throws Exception {
@@ -60,23 +72,6 @@ class AuthControllerTest extends ControllerTest {
     }
 
     @Test
-    void OAuth_로그인_성공하면_200을_반환() throws Exception {
-        // given
-        String provider = "google";
-        String code = "validCode";
-        String loginLink = "https://mocked-auth-url.com";
-        given(authService.oauthLogin(provider, code)).willReturn(loginLink);
-
-        // when & then
-        mockMvc.perform(post("/auth/oauth/{provider}/login", provider)
-                        .param("code", code))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.isSuccess").value(true))
-                .andExpect(jsonPath("$.data").value(loginLink))
-                .andExpect(jsonPath("$.error").doesNotExist());
-    }
-
-    @Test
     void OAuth_로그인_api_호출_시_code_없으면_오류() throws Exception {
         // given
         String provider = "google";
@@ -85,7 +80,42 @@ class AuthControllerTest extends ControllerTest {
         mockMvc.perform(post("/auth/oauth/{provider}/login", provider))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.isSuccess").value(false))
-                .andExpect(jsonPath("$.error.code").value(CommonErrorCode.MISSING_REQUIRED_PARAMETER.getHttpStatus().value()))
-                .andExpect(jsonPath("$.error.message").value(CommonErrorCode.MISSING_REQUIRED_PARAMETER.getMessage()));
+                .andExpect(jsonPath("$.error.code").value(BusinessErrorCode.MISSING_REQUIRED_PARAMETER.getHttpStatus().value()))
+                .andExpect(jsonPath("$.error.message").value(BusinessErrorCode.MISSING_REQUIRED_PARAMETER.getMessage()));
+    }
+
+    @Test
+    void OAuth_로그인_api_호출_시_JWT_토큰_발급() throws Exception {
+        // given
+        String provider = "google";
+        String code = "test";
+        UserTokenDto userTokenDto = new UserTokenDto(ACCESS_TOKEN, REFRESH_TOKEN);
+
+        // when
+        when(authService.oauthLogin(anyString(), anyString())).thenReturn(userTokenDto);
+
+        // then
+        mockMvc.perform(post("/auth/oauth/{provider}/login", provider)
+                        .param("code", code))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.isSuccess").value(true))
+                .andExpect(jsonPath("$.data").value(ACCESS_TOKEN))
+                .andExpect(jsonPath("$.error").doesNotExist());
+    }
+
+    @Test
+    void 리프레시_토큰을_통해_새로운_엑세스_토큰을_발급하면_상태코드_200을_반환() throws Exception {
+        // given
+        UserTokenDto newUserTokenDto = new UserTokenDto("newAccessToken", REFRESH_TOKEN);
+        when(authService.reissue(anyString(), anyString())).thenReturn(newUserTokenDto);
+
+        // when & then
+        mockMvc.perform(post("/auth/reissue")
+                        .cookie(new Cookie("refresh-token", REFRESH_TOKEN))
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + ACCESS_TOKEN))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.isSuccess").value(true))
+                .andExpect(jsonPath("$.data").value("newAccessToken"))
+                .andExpect(jsonPath("$.error").doesNotExist());
     }
 }
---
+++ src/test/java/org/lostnomore/backend/auth/service/AuthServiceTest.java
@@ -0,0 +1,122 @@
+package org.lostnomore.backend.auth.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.lostnomore.backend.auth.dto.UserTokenDto;
+import org.lostnomore.backend.auth.provider.JwtTokenProvider;
+import org.lostnomore.backend.auth.provider.OAuthProvider;
+import org.lostnomore.backend.auth.provider.OAuthProviderFinder;
+import org.lostnomore.backend.auth.repository.RefreshTokenRepository;
+import org.lostnomore.backend.auth.util.BearerAuthorizationExtractor;
+import org.lostnomore.backend.common.ServiceTest;
+import org.lostnomore.backend.global.exception.BusinessException;
+import org.lostnomore.backend.user.domain.SocialType;
+import org.lostnomore.backend.user.domain.User;
+import org.lostnomore.backend.user.manager.UserCreator;
+import org.lostnomore.backend.user.manager.UserRetriever;
+import org.lostnomore.backend.user.service.UserService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest extends ServiceTest {
+
+    @Mock
+    private RefreshTokenRepository refreshTokenRepository;
+
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Mock
+    private OAuthProviderFinder oAuthProviderFinder;
+
+    @Mock
+    private UserRetriever userRetriever;
+
+    @Mock
+    private UserCreator userCreator;
+
+    @Mock
+    private BearerAuthorizationExtractor bearerAuthorizationExtractor;
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private AuthService authService;
+
+    private final String refreshToken = "refresh-token";
+    private final String accessToken = "access-token";
+    private final String provider = "google";
+    private final String oauthCode = "oauth-code";
+    private final String email = "test@example.com";
+    private final String name = "test_name";
+    private final Long userId = 1L;
+    private final SocialType socialType = SocialType.GOOGLE;
+
+    @Test
+    void 기존_유저가_로그인_하면_DB에_저장되지_않는다() {
+        // given
+        OAuthProvider mockOAuthProvider = mock(OAuthProvider.class);
+        when(oAuthProviderFinder.getOAuthProvider(provider)).thenReturn(mockOAuthProvider);
+        when(mockOAuthProvider.getAccessToken(oauthCode)).thenReturn(accessToken);
+        when(mockOAuthProvider.getUserInfo(accessToken)).thenReturn(email);
+
+        User existingUser = User.builder()
+                .email(email)
+                .name(name)
+                .socialType(socialType)
+                .build();
+        User spyUser = spy(existingUser);
+        when(spyUser.getId()).thenReturn(userId);
+
+        when(userRetriever.findByEmailAndSocialType(email, socialType)).thenReturn(spyUser);
+
+        doReturn(new UserTokenDto(accessToken, refreshToken)).when(jwtTokenProvider).createLoginToken(anyLong());
+
+        // when
+        authService.oauthLogin(provider, oauthCode);
+
+        // then
+        verify(userCreator, never()).save(any(User.class));
+    }
+
+    @Test
+    void 토큰들이_만료되지_않으면_기존_토큰_반환() {
+        // given
+        when(bearerAuthorizationExtractor.extractAccessToken(anyString())).thenReturn(accessToken);
+        when(jwtTokenProvider.isValidRefreshAndInvalidAccess(refreshToken, accessToken)).thenReturn(false);
+        when(jwtTokenProvider.isValidRefreshAndValidAccess(refreshToken, accessToken)).thenReturn(true);
+        // when
+        UserTokenDto reissue = authService.reissue(refreshToken, accessToken);
+
+        // then
+        assertThat(reissue.getAccessToken()).isEqualTo(accessToken);
+        assertThat(reissue.getRefreshToken()).isNull();
+    }
+
+    @Test
+    void 토큰들이_모두_만료되면_오류() {
+        // given
+        when(bearerAuthorizationExtractor.extractAccessToken(anyString())).thenReturn(accessToken);
+        when(jwtTokenProvider.isValidRefreshAndInvalidAccess(refreshToken, accessToken)).thenReturn(false);
+        when(jwtTokenProvider.isValidRefreshAndValidAccess(refreshToken, accessToken)).thenReturn(false);
+
+        // when & then
+        assertThatThrownBy(() -> authService.reissue(refreshToken, accessToken))
+                .isInstanceOf(BusinessException.class);
+    }
+}
