0d6fd9e3306eab694348677e62aeb2e8ed80d504
dongyun <ehddbs452100@naver.com>
2025-02-15 01:54:54 +0900

    [refactor] #31 Remover클래스를 바로 이용

--- src/main/java/org/lostnomore/backend/notification/service/NotificationService.java
+++ src/main/java/org/lostnomore/backend/notification/service/NotificationService.java
@@ -1,16 +1,10 @@
 package org.lostnomore.backend.notification.service;
 
 import lombok.RequiredArgsConstructor;
-import org.lostnomore.backend.notification.manager.UserNotificationRemover;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
 
-    private final UserNotificationRemover userNotificationRemover;
-
-    public void deleteByUserId(Long userId) {
-        userNotificationRemover.deleteByUserId(userId);
-    }
 }
--- src/main/java/org/lostnomore/backend/subscribe/service/SubscribeService.java
+++ src/main/java/org/lostnomore/backend/subscribe/service/SubscribeService.java
@@ -1,16 +1,10 @@
 package org.lostnomore.backend.subscribe.service;
 
 import lombok.RequiredArgsConstructor;
-import org.lostnomore.backend.subscribe.manager.SubscribeRemover;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SubscribeService {
 
-    private final SubscribeRemover subscribeRemover;
-
-    public void deleteByUserId(Long userId) {
-        subscribeRemover.deleteByUserId(userId);
-    }
 }
