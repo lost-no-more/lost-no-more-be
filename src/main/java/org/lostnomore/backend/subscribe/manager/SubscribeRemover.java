69dcd9c7bf0b991b15c020a75c8c16e1c244b594
dongyun <ehddbs452100@naver.com>
2025-02-15 16:23:55 +0900

    [fix] #31 필요없는 코드 삭제

--- src/main/java/org/lostnomore/backend/subscribe/manager/SubscribeRemover.java
+++ src/main/java/org/lostnomore/backend/subscribe/manager/SubscribeRemover.java
@@ -2,20 +2,13 @@ package org.lostnomore.backend.subscribe.manager;
 
 import lombok.RequiredArgsConstructor;
 import org.lostnomore.backend.subscribe.repository.SubscribeRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class SubscribeRemover {
 
     private final SubscribeRepository subscribeRepository;
-
-    @Modifying
-    @Transactional
-    @Query("DELETE FROM Subscribe s WHERE s.user.id = :userId")
     public void deleteByUserId(Long userId) {
         subscribeRepository.deleteByUserId(userId);
     }
