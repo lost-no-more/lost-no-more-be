18a012316cb4e6fba95c764385f4b7d25d76a9f6
chaewonkim <kimlove9182@gmail.com>
2025-02-15 14:29:10 +0900

    [feat] #34 인증인가 필요없는 엔드포인트 추가

--- src/main/java/org/lostnomore/backend/global/config/WebMvcConfig.java
+++ src/main/java/org/lostnomore/backend/global/config/WebMvcConfig.java
@@ -23,7 +23,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(new AccessTokenInterceptor(jwtTokenProvider, bearerAuthorizationExtractor))
                 .addPathPatterns("/**")
                 .excludePathPatterns(
-                        "/auth/**"
+                        "/auth/**",
+                        "/test/**",
+                        "/items/search/**",
+                        "/items/count"
                 );
     }
 
