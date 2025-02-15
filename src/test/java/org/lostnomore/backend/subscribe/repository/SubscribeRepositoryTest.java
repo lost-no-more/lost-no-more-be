7e4462c0a72d2311270ed312586baf45e69e28db
chaewonkim <kimlove9182@gmail.com>
2025-02-16 00:58:46 +0900

    [feat] #34 필요없는 import문 삭제

--- src/test/java/org/lostnomore/backend/subscribe/repository/SubscribeRepositoryTest.java
+++ src/test/java/org/lostnomore/backend/subscribe/repository/SubscribeRepositoryTest.java
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 
-import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Optional;
 
