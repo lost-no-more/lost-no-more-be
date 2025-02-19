04a407b287ac86094ffeadb2876f5d0ad6b8b2e0
chaewonkim <kimlove9182@gmail.com>
2025-02-20 02:42:09 +0900

    [feat] #46 테스트할 때 엘라스틱 서치 인덱스가 없어 오류나는 문제 해결

--- src/test/java/org/lostnomore/backend/BackendApplicationTests.java
+++ src/test/java/org/lostnomore/backend/BackendApplicationTests.java
@@ -1,10 +1,15 @@
 package org.lostnomore.backend;
 
 import org.junit.jupiter.api.Test;
+import org.lostnomore.backend.item.elastic.NoriAnalyzerService;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class BackendApplicationTests {
+
+	@MockitoBean
+	private NoriAnalyzerService noriAnalyzerService;
 
 	@Test
 	void contextLoads() {
