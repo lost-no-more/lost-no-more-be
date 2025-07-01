c3f34cf948ebd89fe3fafe74bd947f5bc78c6a9c
bronze_yun_4521 <ehddbs452100@naver.com>
2025-07-01 17:23:28 +0900

    [fix] #73 테스트 코드 수정

--- src/test/java/org/lostnomore/backend/subscribe/repository/SubscribeRepositoryTest.java
+++ src/test/java/org/lostnomore/backend/subscribe/repository/SubscribeRepositoryTest.java
@@ -1,5 +1,10 @@
 package org.lostnomore.backend.subscribe.repository;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,19 +14,11 @@ import org.lostnomore.backend.item.domain.Location;
 import org.lostnomore.backend.item.repository.CategoryRepository;
 import org.lostnomore.backend.item.repository.LocationRepository;
 import org.lostnomore.backend.subscribe.domain.Subscribe;
-import org.lostnomore.backend.user.domain.SocialType;
 import org.lostnomore.backend.user.domain.User;
 import org.lostnomore.backend.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class SubscribeRepositoryTest extends RepositoryTest {
