06b2009332b928efe4b2cd964693cbbcf7702653
bronze_yun_4521 <ehddbs452100@naver.com>
2025-07-05 16:03:33 +0900

    [fix] ElasticSearch 주소 수

--- src/main/java/org/lostnomore/backend/item/elastic/NoriAnalyzerService.java
+++ src/main/java/org/lostnomore/backend/item/elastic/NoriAnalyzerService.java
@@ -1,37 +1,41 @@
 package org.lostnomore.backend.item.elastic;
 
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.ArrayList;
-import java.util.List;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class NoriAnalyzerService {
 
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
-    private static final String ELASTICSEARCH_URL = "http://lost-elasticsearch:9200/lost_item/_analyze";
 
-    public NoriAnalyzerService(RestTemplate restTemplate, ObjectMapper objectMapper) {
-        this.restTemplate = restTemplate;
-        this.objectMapper = objectMapper;
-    }
+    @Value("${spring.elasticsearch.uris}")
+    private String elasticsearchUrl;
 
     public List<String> analyzeKeyword(String keyword) {
         try {
+            String analyzeUrl = elasticsearchUrl + "/lost_item/_analyze";
             String requestJson = "{ \"analyzer\": \"nori_analyzer\", \"text\": \"" + keyword + "\" }";
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
             HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
 
-            ResponseEntity<String> response = restTemplate.postForEntity(ELASTICSEARCH_URL, entity, String.class);
+            ResponseEntity<String> response = restTemplate.postForEntity(analyzeUrl, entity, String.class);
 
             JsonNode rootNode = objectMapper.readTree(response.getBody());
             List<String> tokens = new ArrayList<>();
